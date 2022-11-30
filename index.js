eaec8d634a799edbee1dfc3a6d3711e6ada3440e
hoangREALER <hoangle1742002@gmail.com>
2022-11-30 13:40:22 +0700

    task 5

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get("/")
+app.set('views', './views/templates')
+// app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log("Server starts at: " + PORT))
