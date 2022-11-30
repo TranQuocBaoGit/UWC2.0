2636b48e992f7986d6e3fab7d6c5f091bfad5c49
hoangREALER <hoangle1742002@gmail.com>
2022-11-30 13:54:34 +0700

    task 5

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.set('views', './views/templates')
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.render('index');
